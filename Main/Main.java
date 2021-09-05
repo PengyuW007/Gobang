c3c761faca4110192be9a6b8e6fcb540a145b188
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-05 14:46:10 +0800

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -2,9 +2,85 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        welcome();
-    }
+        //welcome();
+        System.out.println("*************************************************************************");
+        System.out.println("*************************** Welcome to Gomoku ***************************");
+        System.out.println("***************************** 1. Start Game *****************************");
+        System.out.println("***************************** 2. Quit Game ******************************");
+        System.out.println("*************************************************************************");
 
+        System.out.println("Type your choice ");
+        Scanner scan = new Scanner(System.in);
+        int startQuit;
+        Boolean valid = false;
+        do {
+            startQuit = scan.nextInt();
+            if (startQuit == 1) {
+                System.out.println("\n" + "Now start Game!");
+                //Start the game
+                System.out.println("1. PVP" + "\t" + "2. AI");
+                valid = false;
+                do {
+                    int choice = scan.nextInt();
+                    if (choice == 1) {
+                        //PVP
+                        //Player p1 = new Human();
+                        // Player p2 = new Human();
+                        valid = true;
+                    } else if (choice == 2) {
+                        // AI VS Human
+                        // Player p1 = new Human();
+                        //Player p2 = new AI();
+                        valid = true;
+                    } else {
+                        System.out.println("Not valid input. (Type 1 or 2)Try again!");
+                    }
+                } while (!valid);
+
+                /*****************
+                 Initialize Board
+                 ****************/
+
+                System.out.println("Which size of the board you want to play");
+                System.out.println("1. 9*9" + "\t" + "2. 15*15" + "\t" + "3. 19*19");
+
+                valid = false;
+                Game game = null;
+                do {
+                    int choice = scan.nextInt();
+                    if (choice == 1) {
+                        //9*9
+                        game = new Game(9);
+                        valid = true;
+                    } else if (choice == 2) {
+                        //15*15
+                        game = new Game(15);
+                        valid = true;
+                    } else if (choice == 3) {
+                        //19*19
+                        game = new Game(19);
+                        valid = true;
+                    } else {
+                        System.out.println("Not valid input. (Type 1, 2 or 3)Try again!");
+                    }
+                } while (!valid);
+
+
+            } else if (startQuit == 2) {
+                valid = true;
+                System.out.println("See you next time!");
+                System.exit(0);
+            } else {
+                System.out.println("Not valid input. (Type 1 or 2)Try again!");
+            }//end if startquit
+        } while (valid == false);
+
+
+
+
+
+
+    /*
     public static void welcome() {
         System.out.println("*************************************************************************");
         System.out.println("*************************** Welcome to Gomoku ***************************");
@@ -34,8 +110,9 @@ public class Main {
         } while (valid == false);
     }//end welcome
 
-    public static void aiOrPvp() {
+    public static int aiOrPvp() {
         System.out.println("1. PVP"+"\t"+"2. AI");
+        int res = 0;
         Scanner scan = new Scanner(System.in);
         Boolean valid = false;
 
@@ -45,47 +122,53 @@ public class Main {
                 //PVP
                 //Player p1 = new Human();
                // Player p2 = new Human();
+                res = 1;
                 valid = true;
             } else if (choice == 2) {
                 // AI VS Human
                // Player p1 = new Human();
                 //Player p2 = new AI();
+                res = 2;
                 valid = true;
             } else {
                 System.out.println("Not valid input. (Type 1 or 2)Try again!");
             }
         } while (!valid);
-
+        return res;
     }//end aiOrPVP
 
-    public static void initializeBoard() {
+    public static int initializeBoard() {
         System.out.println("Which size of the board you want to play");
         System.out.println("1. 9*9" + "\t" + "2. 15*15" + "\t" + "3. 19*19");
-
+        int size  = 0;
         Scanner scan = new Scanner(System.in);
         Boolean valid = false;
-        Game game = null;
+        //Game game = null;
         do {
             int choice = scan.nextInt();
             if (choice == 1) {
                 //9*9
-                 game = new Game(9);
+                 //game = new Game(9);
+                size = 9;
                 valid = true;
             } else if (choice == 2) {
                 //15*15
-                game = new Game(15);
+                //game = new Game(15);
+                size = 15;
                 valid = true;
             } else if (choice == 3) {
                 //19*19
-               game = new Game(19);
+               //game = new Game(19);
+                size = 19;
                valid = true;
             } else {
                 System.out.println("Not valid input. (Type 1, 2 or 3)Try again!");
             }
         } while (!valid);
-
-        Board board = game.getBoard();
-        board.printBoard();
+        return size;
+        //Board board = game.getBoard();
+        //board.printBoard();
     }//end initializeBoard
-
+*/
+    }
 }
